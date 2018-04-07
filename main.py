89bcac4ee52843e92b8a5b71c21989c2a44c986d
evanwatterud <cod.videos@hotmail.com>
2018-04-07 18:03:47 -0400

    twitter polling working

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from keras.preprocessing.text import Tokenizer
+from keras.models import model_from_json
 import pandas as pd
 import numpy as np
+import twitter
+from birdy.twitter import StreamClient
 
 def setUpTokenizer():
     print('Setting Up Tokenizer...')
@@ -18,13 +21,14 @@ def setUpTokenizer():
     tokenizer.fit_on_texts(x_train)
 
     print('Setup Complete!')
+    return tokenizer
 
 def loadModel():
     print('Loading Prediction Model...')
     # Load json and create model
     jsonFile = open('model.json', 'r')
     loadedJsonModel = jsonFile.read()
-    json_file.close()
+    jsonFile.close()
     loadedModel = model_from_json(loadedJsonModel)
 
     # Load weights into new model
@@ -36,6 +40,42 @@ def loadModel():
 
     return loadedModel
 
+def setUpTwitterConnection(stream=True):
+    api = twitter.Api(consumer_key='wZFb3lcFyezEJuIGUMUvtZGwJ',
+                  consumer_secret='xBiVivqFgEUpwrDSvT0JWCIOUssgktQtpqN41tsM1q9ErnAO3s',
+                  access_token_key='982689083188285440-wIW0kURcR9rMSlB07cDuK59Z6c23pUH',
+                  access_token_secret='nfmjnZxTyo79t4nMqr9xoQJ5lPKnQYxMVZnmkMUWMXpk1')
+
+    streamApi = StreamClient('wZFb3lcFyezEJuIGUMUvtZGwJ',
+                    'xBiVivqFgEUpwrDSvT0JWCIOUssgktQtpqN41tsM1q9ErnAO3s',
+                    '982689083188285440-wIW0kURcR9rMSlB07cDuK59Z6c23pUH',
+                    'nfmjnZxTyo79t4nMqr9xoQJ5lPKnQYxMVZnmkMUWMXpk1')
+
+    if stream:
+        return streamApi
+    else:
+        return api
+
 def main():
-    setUpTokenizer()
-    model = loadModel()
+    # tokenizer = setUpTokenizer()
+    # model = loadModel()
+    twitterApi = setUpTwitterConnection()
+
+    while True:
+        print('Twitter sentiment analizer ready, enter \'quit\' in topic selection to exit')
+        topic = str(input('Select topic: '))
+        if topic == 'quit':
+            break
+        numTweets = int(input('Select number of tweets to analyze: '))
+
+        resource = twitterApi.stream.statuses.filter.post(track=topic)
+
+        index = 0
+        for data in resource.stream():
+            if index >= numTweets:
+                break
+            if data.lang == 'en':
+                index = index + 1
+                print(str(index) + '/' + str(numTweets))
+
+main()
